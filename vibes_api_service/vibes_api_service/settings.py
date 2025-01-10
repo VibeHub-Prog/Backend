226486f550358948193391deae0a232098d30e91
crystal <crystalokd@gmail.com>
2025-01-10 16:32:33 +0100

    ....

--- vibes_api_service/vibes_api_service/settings.py
+++ vibes_api_service/vibes_api_service/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "vibes_api_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
