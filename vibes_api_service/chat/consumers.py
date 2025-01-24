835aa1e8930ef3b2f6ec0cad194f015f04ad6c92
crystal <crystalokd@gmail.com>
2025-01-24 10:37:52 +0100

    ...

--- vibes_api_service/chat/consumers.py
+++ vibes_api_service/chat/consumers.py
@@ -3,37 +3,44 @@ import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.chat_id = self.scope['url_route']['kwargs']['chat_id']
+        self.chat_group_name = f'chat_{self.chat_id}'
 
+        # Join the chat group
         await self.channel_layer.group_add(
-            self.room_group_name,
+            self.chat_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
+        # Leave the chat group
         await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.chat_group_name,
             self.channel_name
         )
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
+        sender = data['sender']
 
+        # Broadcast message to group
         await self.channel_layer.group_send(
-            self.room_group_name,
+            self.chat_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'sender': sender,
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
+        sender = event['sender']
 
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'sender': sender,
         }))
--- vibes_api_service/chat/views.py
+++ vibes_api_service/chat/views.py
@@ -1,12 +1,60 @@
-from rest_framework import serializers, viewsets
-from .models import ChatRoom, Message
-from .serializers import ChatRoomSerializer, MessageSerializer
+from rest_framework import generics, status, serializers
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Chat, Message
+from .serializers import ChatSerializer, MessageSerializer
+from django.db.models import Q
 
 
-class ChatRoomViewSet(viewsets.ModelViewSet):
-    queryset = ChatRoom.objects.all()
-    serializer_class = ChatRoomSerializer
+class ChatListView(generics.ListCreateAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+    # permission_classes = [IsAuthenticated]
 
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
+    def perform_create(self, serializer):
+        participants = self.request.data.get('participants', [])
+        if not isinstance(participants, list):
+            raise serializers.ValidationError({'error': 'Participants must be a list of user IDs.'})
+        if len(participants) != 2:
+            raise serializers.ValidationError({'error': 'A chat must have exactly two participants.'})
+        # if self.request.user.id not in participants:
+        #     raise serializers.ValidationError({'error': 'You can only create chats that include yourself as a participant.'})
+        participants = sorted(participants)
+        existing_chat = Chat.objects.filter(
+            Q(participants=participants[0]) & Q(participants=participants[1]) |
+            Q(participants=participants[1]) & Q(participants=participants[0])
+        ).distinct()
+        if existing_chat.exists():
+            raise serializers.ValidationError({'error': 'A chat between these participants already exists.'})
+        serializer.save()
+
+
+
+class MessageListView(generics.ListCreateAPIView):
     serializer_class = MessageSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Message.objects.filter(chat__id=self.kwargs['chat_id']).order_by('timestamp')
+
+    def perform_create(self, serializer):
+        chat = Chat.objects.get(id=self.kwargs['chat_id'])
+        serializer.save(chat=chat, sender=self.request.user)
+
+
+
+
+
+
+# from rest_framework import serializers, viewsets
+# from .models import ChatRoom, Message
+# from .serializers import ChatRoomSerializer, MessageSerializer
+
+
+# class ChatRoomViewSet(viewsets.ModelViewSet):
+#     queryset = ChatRoom.objects.all()
+#     serializer_class = ChatRoomSerializer
+
+# class MessageViewSet(viewsets.ModelViewSet):
+#     queryset = Message.objects.all()
+#     serializer_class = MessageSerializer
